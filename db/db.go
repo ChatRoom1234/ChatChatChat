8e49a90bfe4ac6aca0d2da78f113e95efaeb265f
Vasilii Bodnariuk <i@vasilesk.ru>
2017-08-25 23:53:00 +0300

    temp conn problem fix

--- db/db.go
+++ db/db.go
@@ -48,6 +48,7 @@ func getConf() conf {
 func CreateKey(userID int) string {
 	key := randKey()
 
+	db := getDbConn()
 	db.QueryRow("INSERT INTO access_keys(user_id,key) VALUES($1,$2);", userID, key)
 
 	return key
@@ -55,6 +56,7 @@ func CreateKey(userID int) string {
 
 // GetUserByKey returns user login by key or "" if no user with the key exists
 func GetUserByKey(key string) string {
+	db := getDbConn()
 	rows, err := db.Query("SELECT login FROM access_keys JOIN users ON user_id=id WHERE key=$1 LIMIT 1;", key)
 	checkErr(err)
 
@@ -70,6 +72,7 @@ func GetUserByKey(key string) string {
 
 // CreateUser adds user to database
 func CreateUser(login string, password string) (int, error) {
+	db := getDbConn()
 	rows, err := db.Query("SELECT COUNT(*) FROM users WHERE login=$1 LIMIT 1;", login)
 	checkErr(err)
 	for rows.Next() {
@@ -92,6 +95,7 @@ func CreateUser(login string, password string) (int, error) {
 
 // ValidateUser checks if user with login-password exists returning id or zero if doesn`t
 func ValidateUser(login string, password string) int {
+	db := getDbConn()
 	rows, err := db.Query("SELECT id, password FROM users WHERE login=$1 LIMIT 1;", login)
 	checkErr(err)
 
@@ -116,6 +120,16 @@ func initDb() error {
 	db, err = sql.Open("postgres", dbinfo)
 
 	return err
+}
+
+func getDbConn() *sql.DB {
+	fmt.Println("separate db conn")
+	c := getConf()
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable port=5432 host=vasilesk.ru",
+		c.User, c.Password, c.Database)
+	conn, _ := sql.Open("postgres", dbinfo)
+
+	return conn
 }
 
 func init() {
